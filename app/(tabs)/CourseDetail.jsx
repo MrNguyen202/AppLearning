4d46eaf0e02153371a78aa6968a619984bc2a262
nguyentu494 <nguyentu2003.khtgdd@gmail.com>
2024-11-06 17:31:28 +0700

    commit video chua hoan thien detail page

--- app/(tabs)/CourseDetail.jsx
+++ app/(tabs)/CourseDetail.jsx
@@ -4,6 +4,7 @@ import { createMaterialTopTabNavigator } from "@react-navigation/material-top-ta
 import Search from "./Search";
 import Home from "./Home";
 import { NavigationContainer } from "@react-navigation/native";
+import Video, {VideoRef} from 'react-native-video';
 
 const Tab = createMaterialTopTabNavigator();
 
@@ -26,6 +27,12 @@ const CourseDetail = ({ navigation, route }) => {
   return (
     <View>
       <View className={`w-full h-[200] bg-slate-500`}></View>
+      {/* <Video
+        source={{ uri: 'https://www.example.com/path-to-video.mp4' }} // URL hoặc đường dẫn file
+        style={{width: '100%', height: '100%'}} // Kích thước video
+        resizeMode="contain" // Tùy chọn hiển thị video
+      /> */}
+
       <View>
         <Text>abc</Text>
       </View>
