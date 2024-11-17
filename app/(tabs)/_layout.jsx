4e26ee7a6ad15d6bb7491b68624194496c19d7b5
nguyenthuan202 <nguyenthuan020010@gmail.com>
2024-11-17 22:07:59 +0700

    test rename file

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -8,7 +8,7 @@ import Profile from './Profile'
 import Search from './Search'
 import PaymentMethod from './PaymentMethod'
 import PaymentSuccess from '././PaymentSuccess'
-import Home from './Home';
+import Home from './Home_test';
 
 const TabIcon = ({icon, color, name, focused}) => {
     return (
