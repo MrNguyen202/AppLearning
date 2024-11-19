955854cdba38f8c6bb9d9e4d1ccd4e3702661650
nguyentu494 <nguyentu2003.khtgdd@gmail.com>
2024-11-19 15:22:10 +0700

    fix login backend

--- controllers/user_controller.ts
+++ controllers/user_controller.ts
@@ -2,7 +2,9 @@ import userModels from "../models/user_models";
 
 const userController = {
     checkLogin: async (email: string, password: string) => {
-        return await userModels.checkLogin(email, password);
+        const data = await userModels.checkLogin(email, password);
+        console.log(data)
+        return data;
     }
 }
 
--- models/user_models.ts
+++ models/user_models.ts
@@ -15,20 +15,15 @@ const userModels = {
               },
             }
           );
-          console.log(response.data)
-          return response.data; // Trả về dữ liệu nếu đăng nhập thành công
+          return response.data; 
         } catch (error: any) {
-          // Xử lý lỗi một cách rõ ràng hơn
           if (error.response) {
-            // Lỗi từ server (4xx hoặc 5xx)
             console.error('Lỗi từ server:', error.response.data.message);
             throw new Error(error.response.data.message || 'Đăng nhập thất bại');
           } else if (error.request) {
-            // Không nhận được phản hồi từ server
             console.error('Không nhận được phản hồi từ server:', error.request);
             throw new Error('Không thể kết nối tới server');
           } else {
-            // Lỗi khác
             console.error('Lỗi khác:', error.message);
             throw new Error(error.message);
           }
