e1d6fa1194a72469f808db33e93cfeb92f5f77fd
nguyenthuan202 <nguyenthuan020010@gmail.com>
2024-11-23 01:39:05 +0700

    update image course is image-not-found if course image null

--- components/CourseWatching.js
+++ components/CourseWatching.js
@@ -11,7 +11,7 @@ const CourseWatching = ({ item, getCourse }) => {
   return (
     <TouchableOpacity onPress={() => handleGetCourse(item.courseId)} key={item.courseId} className={"my-2 px-2 mr-[11] h-56 w-44 justify-evenly bg-white rounded-lg"}>
       <View>
-        <Image source={{ uri: item.courseImage }} className="w-40 h-28 rounded-lg" />
+        <Image source={{ uri: item.courseImage === null ? "https://res.cloudinary.com/dx0blzlhd/image/upload/v1732300310/appELearning/image-not-found_mdolip.jpg" : item.courseImage  }} className="w-40 h-28 rounded-lg" />
         <Text className={`absolute ml-2 mt-1 text-white bg-[#FD853A] rounded text-xs pl-2 pr-2 pt-1 pb-1 font-bold `}>{item.status}</Text>
       </View>
       <Text className="font-bold">{item.courseName}</Text>
