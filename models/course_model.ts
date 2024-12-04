4262f4891f110d0399261a0a840f6e304443694d
nguyentu494 <nguyentu2003.khtgdd@gmail.com>
2024-12-04 08:32:35 +0700

    push fix recommend

--- models/course_model.ts
+++ models/course_model.ts
@@ -119,7 +119,26 @@ const courseModels = {
                 throw new Error(error.message);
             }
         }
-    }
+    },
+
+    //get recommend course
+    getRecommendCourses: async (userId: string) => {
+      try {
+          const response = await axios.get(`http://10.0.2.2:8080/api/v1/courses/recommend/${userId}`);
+          return response.data;
+      } catch (error: any) {
+          if (error.response) {
+              console.error('Lỗi từ server:', error.response.data.message);
+              throw new Error(error.response.data.message || 'Lấy danh sách khóa học thất bại');
+          } else if (error.request) {
+              console.error('Không nhận được phản hồi từ server:', error.request);
+              throw new Error('Không thể kết nối tới server');
+          } else {
+              console.error('Lỗi khác:', error.message);
+              throw new Error(error.message);
+          }
+      }
+  }
 };
 
 export default courseModels;
